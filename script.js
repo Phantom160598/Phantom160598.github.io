5469500c8c113912110a98b3dc2e8645cb14c3ee
Phantom160598 <79705012+Phantom160598@users.noreply.github.com>
2023-07-26 13:48:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const closeButton = document.getElementById("close");
 helpButton.addEventListener("click", function() {
 helpOverlay.style.display = "block";
 closeButton.style.display = "block";
-background.reload; 
+background.reload(); 
 });
 
 closeButton.addEventListener("click", function() {
